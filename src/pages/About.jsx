2f8acbf4b54e71c76d0ffdd125be4a62d114b6da
klaudio toska <klaudyotoska@gmail.com>
2023-02-28 21:05:46 +0100

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. This
+        project is part of the ikubINFO Course
+       
+        
+        
+        <strong>
+                    Klaudio Toska
+        </strong>
+        .
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+       
+      </p>
+    </>
+  )
+}
+
+export default About
